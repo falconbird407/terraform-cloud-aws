3291c692530214229d09dc3b6a86168980767d59
Nima Ziaee <nima.ziaee@gmail.com>
2026-05-15 18:10:36 +0200

    Remove port 443

--- main.tf
+++ main.tf
@@ -17,16 +17,6 @@ resource "aws_security_group" "linux_lab_sg" {
   }
 
   ingress {
-    description = "HTTPS"
-
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "HTTP"
 
     from_port   = 80
